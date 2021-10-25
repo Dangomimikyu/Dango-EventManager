6d23f8bfe1a1e50e69332f50acb25aeb2bc66b88
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2021-10-26 01:43:32 +0800

    im too tired, gonna continue tomorrow

---
+++ Assets/Scripts/EventArgumentData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DangoMimikyu.Events
+{
+    public interface IEventArgData
+    {
+        string eventName { get; } // name of the event to listen to, this variable is set as a read-only accessor
+    }
+
+
+    public class EventArgumentData<T> : IEventArgData
+    {
+        public string eventName { private set; get; }
+        public T otherArguments;
+
+        public EventArgumentData(string EventToSub, T otherArgs)
+        {
+            eventName = EventToSub;
+            otherArguments = otherArgs;
+        }
+    }
+}
--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    public static event Action<int> testAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //testAction.method
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
